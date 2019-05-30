75a0af49a93cdba910c3d4192656a2e255de1481
Bob Tanner <tanner@real-time.com>
2019-05-30 10:57:45 -0500

    Refactor to use Install-ChocolateyPackage

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,36 +5,27 @@ $version     = '452'
 $fullPackage = $packageName + $version + '.exe'
 $url         = 'http://stevengould.org/downloads/cleanup/' + $fullPackage
 $checkSum    = '6ac7576c0b48ddee292f85724c7917e11360927ff8e5c5a8d795577a4241c131'
-
-$WorkSpace = Join-Path $env:TEMP "$packageName-$env:chocolateyPackageVersion"
-
-$WebFileArgs = @{
-   packageName  = $packageName
-   FileFullPath = Join-Path $WorkSpace $fullPackage
-   Url          = $url
-   Checksum     = $checkSum
-   ChecksumType = 'sha256'
-   GetOriginalFileName = $true
-}
-
-$PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
+$workSpace   = Join-Path $env:TEMP "$packageName-$env:chocolateyPackageVersion"
 
 # silent install requires AutoIT
 #
-$autoitExe = 'AutoIt3.exe'
-$toolsDir    = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$autoitFile = Join-Path $toolsDir 'cleanup.au3'
-$fileFullPath = Join-Path $WorkSpace $fullPackage
-Write-Debug "$autoitFile"
-Write-Debug "$fileFullPath"
-Start-Process  -FilePath $autoitExe -ArgumentList "$autoitFile $fileFullPath" -PassThru
+$autoitExe    = 'AutoIt3.exe'
+$toolsDir     = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$autoitFile   = Join-Path $toolsDir 'cleanup.au3'
+$fileFullPath = Join-Path $workSpace $fullPackage
 
-$InstallArgs = @{
-   PackageName    = $packageName
-   File           =  Join-Path $WorkSpace $fullPackage
+$autoitProcess = Start-Process -FilePath $autoitExe -ArgumentList "$autoitFile $fileFullPath" -PassThru
+
+$installArgs = @{
+   softwareName   = 'CleanUp*'
+   packageName    = $packageName
+   file           =  Join-Path $WorkSpace $fullPackage
+   url            = $url
+   checksum       = $checkSum
+   checksumType   = 'sha256'
    fileType       = 'exe'
    silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName)-$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1"
    validExitCodes = @(0, 3010, 1641)
 }
 
-Install-ChocolateyInstallPackage @InstallArgs
+Install-ChocolateyPackage @installArgs
